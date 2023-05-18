79a5c36ac1f6590b4a545ce0a0fb4c4baf5f3944
Semyon_Trushik <semyontrushik@gmail.com>
2023-05-18 15:31:58 +0300

    correcting the comments and added the function of creating posts

--- src/App.vue
+++ src/App.vue
@@ -5,25 +5,11 @@
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-// import EmptyAccount from './components/Empty-account.vue';
-
 export default {
   name: 'App',
-  // components: {
-    // HelloWorld,
-    // EmptyAccount,
-  // },
 }
 </script>
 
 <style>
-html {
-  width: 100%;
-}
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+@import url('./style/main.scss');
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,67 +1,39 @@
 <template>
   <div class="registration-form">
-    <form @submit.prevent="register" >
+    <form @submit.prevent="register">
       <input type="email" v-model="email" placeholder="Email" />
       <input type="password" v-model="password" placeholder="Пароль" />
-      <button type="submit">Зарегистрироваться</button>
+      <button type="submit">Registration</button>
     </form>
   </div>
 </template>
 
 <script>
-import axios from "axios"
+import { createUser } from '../services/api'
+// import axios from 'axios'
 export default {
   data() {
     return {
       email: '',
-      password: ''
-    };
+      password: '',
+    }
   },
   methods: {
     register() {
-      axios.post('http://localhost:8080/api/auth/sign-up', { email: this.email, password: this.password })
-        .then(response => {
-          const sessionToken = response;
-          console.log(sessionToken)
-          localStorage.setItem('sessionToken', sessionToken.data.token);
-          this.$router.push('/account')
-          // Редирект на желаемую страницу или выполнение других действий
-        })
-        .catch(error => {
-          console.error(error);
-          // Обработка ошибок
-        });
-    }
-  }
-};
+      const { email, password } = this;
+      createUser(email, password).then((response) => {
+        console.log(response.data.token)
+        this.$router.push('/account')
+        return localStorage.setItem('sessionToken', response.data.token)
+      })
+      .catch((error) => {
+          console.error(error)
+      })
+    },
+  },
+}
 </script>
 
 <style>
-.registration-form {
-  max-width: 400px;
-  margin: 0 auto;
-}
 
-input {
-  width: 100%;
-  padding: 10px;
-  margin-bottom: 10px;
-}
-
-button {
-  width: 100%;
-  padding: 10px;
-  background-color: #4CAF50;
-  color: white;
-  border: none;
-}
-
-button:hover {
-  background-color: #45a049;
-}
-
-button:disabled {
-  background-color: lightgray;
-  cursor: not-allowed;
-}
 </style>
