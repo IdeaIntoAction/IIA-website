8b5837a0771f2e0ec5b47ffaec7046541693776f
Semyon_Trushik <semyontrushik@gmail.com>
2023-09-22 18:50:22 +0300

    create app

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import StatisticCorePage from './pages/StatisticCorePage.vue'
+</script>
+
+<template>
+  <StatisticCorePage/>
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ src/pages/StatisticCorePage.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  date() {}
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+</style>
