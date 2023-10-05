7b15c7332b10ff920c921b8a3a4e76d7ee9bf4cc
Semyon_Trushik <semyontrushik@gmail.com>
2023-10-05 20:15:26 +0300

    filter fic

--- src/pages/StatisticCore.vue
+++ src/pages/StatisticCore.vue
@@ -5,6 +5,11 @@ export default {
   data() {
     return {
       parsers: [],
+      filterConditions: {
+        act: false,
+        stop: false,
+        work: false,
+      }
     };
   },
   mounted() {
@@ -15,6 +20,17 @@ export default {
       .catch((error) => {
         catchError.call(this, error);
       });
+  },
+  computed: {
+    filteredParsers() {
+      return this.parsers.filter(parser => {
+        return (
+          (!this.filterConditions.act || parser.act) &&
+          (!this.filterConditions.stop || parser.stop) &&
+          (!this.filterConditions.work || parser.work)
+        );
+      });
+    },
   },
 };
 </script>
@@ -42,26 +58,34 @@ export default {
           <th class="text-center"><input class="h-12 px-1 border border-slate-700 font-normal" type="text"></th>
           <th class="text-center"><input class="h-12 px-1 border border-slate-700 font-normal" type="text"></th>
           <th class="text-center w-auto"></th>
-          <th class="text-center w-10 h-10"><input type="checkbox"></th>
+          <th class="text-center w-10 h-10"><input v-model="filterConditions.act" type="checkbox"></th>
           <th class="text-center"><input class="h-12 px-1 border border-slate-700 font-normal" type="text"></th>
           <th class="text-center"><input class="h-12 px-1 border border-slate-700 font-normal" type="text"></th>
           <th class="text-center w-10"></th>
-          <th class="text-center w-10 h-10"><input type="checkbox"></th>
-          <th class="text-center w-10 h-10"><input type="checkbox"></th>
+          <th class="text-center w-10 h-10"><input v-model="filterConditions.stop" type="checkbox"></th>
+          <th class="text-center w-10 h-10"><input v-model="filterConditions.work" type="checkbox"></th>
         </tr>
       </thead>
-      <tbody v-for="parser in parsers" :key="parser.id" class="mt-4">
+      <tbody v-for="parser in filteredParsers" :key="parser.id" class="mt-4">
         <tr class="text-center h-10">
-          <td class="truncate border border-slate-300 px-2"><a href={{parser.site.link}}>{{ parser.site }}</a></td>
+          <td class="truncate border border-slate-300 px-2"><a :href="parser.site.link">{{ parser.site }}</a></td>
           <td class="truncate border border-slate-300 px-2">Load</td>
           <td class="truncate border border-slate-300 px-2">Start</td>
-          <td class="truncate border border-slate-300 px-2"><router-link class="text-blue-600" :to="{ name: 'parserInfo', params: { id: parser.id } }">Link</router-link></td>
-          <td class="truncate border border-slate-300"><input type="checkbox"></td>
+          <td class="truncate border border-slate-300 px-2">
+            <router-link class="text-blue-600" :to="{ name: 'parserInfo', params: { id: parser.id } }">Link</router-link>
+          </td>
+          <td class="truncate border border-slate-300">
+            <input v-model="parser.act" type="checkbox">
+          </td>
           <td class="truncate border border-slate-300 px-2">Error</td>
           <td class="truncate border border-slate-300 px-2">Comm</td>
           <td class="truncate border border-slate-300 px-2 text-end">Late</td>
-          <td class="truncate border border-slate-300"><input type="checkbox"></td>
-          <td class="truncate border border-slate-300"><input type="checkbox"></td>
+          <td class="truncate border border-slate-300">
+            <input v-model="parser.stop" type="checkbox" >
+          </td>
+          <td class="truncate border border-slate-300">
+            <input v-model="parser.work" type="checkbox">
+          </td>
         </tr>
       </tbody>
     </table>
